3601b9df0b21fc5c11a6ff8e6b398da2ce7f4c96
Julia <109692652+Astra314@users.noreply.github.com>
2023-02-17 10:41:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-# python3
-
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
-
-def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
-
-if __name__ == "__main__":
-    main()
+# Check for balanced parentheses in an expression
+open_list = ["[","{","("]
+close_list = ["]","}",")"]
+ 
+# Function to check parentheses
+def check(myStr):
+    stack = []
+    for i in range(len(myStr)):
+        if myStr[i] in open_list:
+            stack.append((myStr[i], i))
+        elif myStr[i] in close_list:
+            pos = close_list.index(myStr[i])
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[-1][0])):
+                stack.pop()
+            else:
+                return i+1
+    
+    # print("The stack is: ", stack)
+    if len(stack) == 0:
+        return "Success"
+    else:
+        return stack[-1][1]+2
+    
+ 
+ 
+# Driver code
+string = "{{{}}}"
+print(check(string))
+ 
+string = "h[{}{})(]"
+print(check(string))
+ 
+string = "{[}"
+print(check(string))
